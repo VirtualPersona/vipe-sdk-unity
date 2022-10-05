168f29f3c320fa9b5e34ba418561bb0f8f9ac8a3
CarlosPolygonalMind <carlos.castro@polygonalmind.com>
2022-10-05 11:24:21 +0200

    script to compile

---
+++ Assets/com.cryptoavatars.sdk/Scripts_DoNotIncludeInPackage/CardAvatarController.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CardAvatarController : MonoBehaviour
+{
+
+    private string vrmUrl;
+    private int index;
+    private Texture2D textureAvatar;
+
+    private System.Action<string> onClickCardBtn;
+
+    public void SetAvatarData(string name, string vrmUrl, int index, System.Action<string> onClickCardBtn)
+    {
+        this.onClickCardBtn = onClickCardBtn;
+        this.vrmUrl = vrmUrl;
+        this.index = index;
+
+        GetComponentInChildren<Text>().text = name;
+        Button btnPreview = GetComponentInChildren<Button>();
+        btnPreview.onClick.AddListener(ClickPreview);
+    }
+
+    public void LoadAvatarImage(Texture2D texture)
+    {
+        this.textureAvatar = texture;
+        RawImage imageAvatar = GetComponentInChildren<RawImage>();
+        imageAvatar.texture = this.textureAvatar;
+    }
+
+    private void ClickPreview()
+    {
+        this.onClickCardBtn(this.vrmUrl);
+    }
+
+}
---
+++ Assets/com.cryptoavatars.sdk/Scripts_DoNotIncludeInPackage/Structs.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Structs
+{
+    [System.Serializable]
+    public struct LoginRequestDto
+    {
+        public string email;
+        public string password;
+    }
+
+    [System.Serializable]
+    public struct LoginResponseDto
+    {
+        public string userId;
+        public string wallet;
+    }
+
+    [System.Serializable]
+    public struct DefaultSearchAvatarsDto
+    {
+        public string collectionAddress;
+        public string license;
+    }
+
+    public struct OwnerAvatarsDto
+    {
+        public string collectionAddress;
+        public string owner;
+    }
+
+    [System.Serializable]
+    public struct AvatarsArray
+    {
+        public Avatar[] avatars;
+    }
+
+    [System.Serializable]
+    public struct Avatar
+    {
+        public Metadata metadata;
+
+        [System.Serializable]
+        public struct Metadata
+        {
+            public string name;
+            public string description;
+            public string image;
+            public string asset;
+            public string createdBy;
+            public string createdAt;
+            public string[] tags;
+        }
+    }
+
+    [System.Serializable]
+    public struct NftsArray
+    {
+        public Nft[] nfts;
+        public int totalNfts;
+        public int page;
+        public string next;
+        public string prev;
+    }
+
+    [System.Serializable]
+    public struct Nft
+    {
+        public Metadata metadata;
+
+        [System.Serializable]
+        public struct Metadata
+        {
+            public string name;
+            public string description;
+            public string image;
+            public string asset;
+            public string createdBy;
+            public string createdAt;
+            public string[] tags;
+        }
+    }
+
+
+}
