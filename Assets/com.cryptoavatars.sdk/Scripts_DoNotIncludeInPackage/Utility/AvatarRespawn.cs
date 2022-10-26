bb973dcd4a45bc2c5efe6c378bbc440306fc32bb
notbloodmask <114909420+notbloodmask@users.noreply.github.com>
2022-10-26 16:22:24 +0200

    Updated Avatar Respawn

--- Assets/com.cryptoavatars.sdk/Scripts_DoNotIncludeInPackage/Utility/AvatarRespawn.cs
+++ Assets/com.cryptoavatars.sdk/Scripts_DoNotIncludeInPackage/Utility/AvatarRespawn.cs
@@ -6,7 +6,7 @@ public class AvatarRespawn : MonoBehaviour
 {
     [SerializeField]
     private GameObject VRM_TARGET;
-
+    
     private void OnCollisionEnter(Collision collision)
     {
         collision.transform.position = VRM_TARGET.transform.position;
