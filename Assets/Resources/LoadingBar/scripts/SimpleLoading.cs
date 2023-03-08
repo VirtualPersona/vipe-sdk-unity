415d7f5dbb753be7cdc7f9d7e23edc1c5c394cd0
luixxma <86054977+luixxma@users.noreply.github.com>
2023-03-08 13:43:01 +0100

    Cleaned more unused assets

--- Assets/Resources/LoadingBar/scripts/SimpleLoading.cs
+++ Assets/Resources/LoadingBar/scripts/SimpleLoading.cs
@@ -4,13 +4,11 @@ using UnityEngine.UI;
 public class SimpleLoading : MonoBehaviour {
 
     private RectTransform rectComponent;
-    private Image imageComp;
     public float rotateSpeed = 200f;
 
     // Use this for initialization
     void Start () {
         TryGetComponent(out rectComponent);
-        rectComponent.TryGetComponent(out imageComp);
     }
 	
 	// Update is called once per frame
